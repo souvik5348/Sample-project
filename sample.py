9d734236784e51a0919a6dfd0973a75ae2de72e8
Souvik Das <144410471+souvik5348@users.noreply.github.com>
2025-04-10 20:43:10 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,9 @@
+# Input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Adding the two numbers
+sum = num1 + num2
+
+# Displaying the result
+print("The sum is:", sum)
